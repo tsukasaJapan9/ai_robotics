595ae232cc13a1e35d2e462c4f9e17ee52c04839
Tsukasa Horinouchi <tsukasa.horinouchi@turing-motors.com>
2025-04-24 11:22:27 +0900

    fix

--- pc/led_matrix_client.py
+++ pc/led_matrix_client.py
@@ -1,14 +1,18 @@
 import serial
 
 LED_MAX_BRIGHTNESS = 32
+BRIGHTNESS_DIVIDER = 2
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 
 # 送信するRGBデータ
 rgb_data: list[int] = []
 for i in range(64):
-    r = 10
-    g = 0
-    b = 0
+    r = 0
+    g = i * 2
+    b = i * 2
+    r = int(r // BRIGHTNESS_DIVIDER)
+    g = int(g // BRIGHTNESS_DIVIDER)
+    b = int(b // BRIGHTNESS_DIVIDER)
     rgb_data.extend([r, g, b])
 
 ser.write(bytearray(rgb_data))
---
+++ pc/standup.py
@@ -0,0 +1,108 @@
+############################################
+# ロボットに組み付けた状態でのサーボテスト
+############################################
+import time
+
+from servo import (
+    create_hander,
+    get_drive_mode,
+    get_operating_mode,
+    set_drive_mode,
+    set_operating_mode,
+    set_position,
+    set_torque,
+)
+
+CMD_NORMAL_MODE = 0x00
+CMD_REVERSE_MODE = 0x01
+CMD_VELOCITY_CONTROL_MODE = 0x01
+CMD_POSITION_CONTROL_MODE = 0x03
+
+RIGHT_LEG_ID = 2
+LEFT_LEG_ID = 1
+RIGHT_WHEEL_ID = 3
+LEFT_WHEEL_ID = 4
+
+
+def main() -> None:
+    port_handler, packet_handler = create_hander()
+    if port_handler is None or packet_handler is None:
+        return
+
+    # トルクを抜く
+    set_torque(port_handler, packet_handler, RIGHT_LEG_ID, False)
+    set_torque(port_handler, packet_handler, LEFT_LEG_ID, False)
+    set_torque(port_handler, packet_handler, RIGHT_WHEEL_ID, False)
+    set_torque(port_handler, packet_handler, LEFT_WHEEL_ID, False)
+    time.sleep(1)
+
+    # 制御モードを変更
+    set_operating_mode(
+        port_handler, packet_handler, RIGHT_LEG_ID, CMD_POSITION_CONTROL_MODE
+    )
+    set_operating_mode(
+        port_handler, packet_handler, LEFT_LEG_ID, CMD_POSITION_CONTROL_MODE
+    )
+    set_operating_mode(
+        port_handler, packet_handler, RIGHT_WHEEL_ID, CMD_VELOCITY_CONTROL_MODE
+    )
+    set_operating_mode(
+        port_handler, packet_handler, LEFT_WHEEL_ID, CMD_VELOCITY_CONTROL_MODE
+    )
+
+    time.sleep(2)
+
+    # 制御モードを取得して表示
+    _, val = get_operating_mode(port_handler, packet_handler, RIGHT_LEG_ID)
+    print(f"ID: {RIGHT_LEG_ID}, operating mode: {val}")
+    _, val = get_operating_mode(port_handler, packet_handler, LEFT_LEG_ID)
+    print(f"ID: {LEFT_LEG_ID}, operating mode: {val}")
+    _, val = get_operating_mode(port_handler, packet_handler, RIGHT_WHEEL_ID)
+    print(f"ID: {RIGHT_WHEEL_ID}, operating mode: {val}")
+    _, val = get_operating_mode(port_handler, packet_handler, LEFT_WHEEL_ID)
+    print(f"ID: {LEFT_WHEEL_ID}, operating mode: {val}")
+
+    # ドライブモードを設定
+    set_drive_mode(port_handler, packet_handler, RIGHT_LEG_ID, CMD_NORMAL_MODE)
+    set_drive_mode(port_handler, packet_handler, LEFT_LEG_ID, CMD_REVERSE_MODE)
+    set_drive_mode(port_handler, packet_handler, RIGHT_WHEEL_ID, CMD_NORMAL_MODE)
+    set_drive_mode(port_handler, packet_handler, LEFT_WHEEL_ID, CMD_REVERSE_MODE)
+
+    time.sleep(2)
+
+    # ドライブモードを取得して表示
+    _, val = get_drive_mode(port_handler, packet_handler, RIGHT_LEG_ID)
+    print(f"ID: {RIGHT_LEG_ID}, drive mode: {val}")
+    _, val = get_drive_mode(port_handler, packet_handler, LEFT_LEG_ID)
+    print(f"ID: {LEFT_LEG_ID}, drive mode: {val}")
+    _, val = get_drive_mode(port_handler, packet_handler, RIGHT_WHEEL_ID)
+    print(f"ID: {RIGHT_WHEEL_ID}, drive mode: {val}")
+    _, val = get_drive_mode(port_handler, packet_handler, LEFT_WHEEL_ID)
+    print(f"ID: {LEFT_WHEEL_ID}, drive mode: {val}")
+
+    # トルクを入れる
+    set_torque(port_handler, packet_handler, RIGHT_LEG_ID, True)
+    set_torque(port_handler, packet_handler, LEFT_LEG_ID, True)
+    set_torque(port_handler, packet_handler, RIGHT_WHEEL_ID, True)
+    set_torque(port_handler, packet_handler, LEFT_WHEEL_ID, True)
+
+    # wheelを回す
+    # set_velocity(port_handler, packet_handler, RIGHT_WHEEL_ID, -60)
+    # set_velocity(port_handler, packet_handler, LEFT_WHEEL_ID, -60)
+
+    deg = 205
+    # deg = 215 + 30
+    set_position(port_handler, packet_handler, RIGHT_LEG_ID, deg)
+    set_position(port_handler, packet_handler, LEFT_LEG_ID, deg)
+
+    # time.sleep(10)
+
+    # # トルクを抜く
+    # set_torque(port_handler, packet_handler, RIGHT_LEG_ID, False)
+    # set_torque(port_handler, packet_handler, LEFT_LEG_ID, False)
+    # set_torque(port_handler, packet_handler, RIGHT_WHEEL_ID, False)
+    # set_torque(port_handler, packet_handler, LEFT_WHEEL_ID, False)
+
+
+if __name__ == "__main__":
+    main()
