058da46ac9886b8038f1c0614d630af066b7a230
Tsukasa Horinouchi <tsukasa.horinouchi@turing-motors.com>
2025-05-04 22:37:38 +0900

    調整中

--- m5stack/pen_robo/src/main.cpp
+++ m5stack/pen_robo/src/main.cpp
@@ -30,6 +30,20 @@ using namespace ControlTableItem;
 
 bool controlEnable = false;
 
+// 制御パラメータ(要調整)
+#define MAX_RPM 120.0
+float Kp = 200.0, Ki = 0.0, Kd = 200;
+float scale = 0.1;
+
+float last_error;
+float integral = 0;
+
+bool is_first = true;
+float target_angle = 0;
+
+// 処理時間計測
+float prev_time = millis();
+
 void control_torque(bool enable)
 {
   if (enable)
@@ -85,27 +99,35 @@ void loop()
 {
   M5.update();
 
-  if (M5.BtnC.wasPressed())
-  {
-    controlEnable = !controlEnable;
-    if (controlEnable)
-    {
-      control_torque(true);
-    }
-    else
-    {
-      control_torque(false);
-    }
-  }
-
   float gx, gy, gz;
   float ax, ay, az;
 
   M5.Imu.getGyro(&gx, &gy, &gz);
   M5.Imu.getAccel(&ax, &ay, &az);
 
+  // 姿勢(直立時にゼロ、前方向に倒れた時にプラス、後ろ方向に倒れた時にマイナス)
+  float angle = atan2(ay, az) * 180 / PI;
+
+  if (M5.BtnC.wasPressed())
+  {
+    controlEnable = !controlEnable;
+    if (controlEnable)
+    {
+      control_torque(true);
+      dxl.setGoalPosition(RIGHT_LEG_ID, 205, UNIT_DEGREE);
+      dxl.setGoalPosition(LEFT_LEG_ID, 205, UNIT_DEGREE);
+      target_angle = angle;
+      M5.Lcd.setTextColor(WHITE, DARKGREY);
+    }
+    else
+    {
+      control_torque(false);
+      M5.Lcd.setTextColor(WHITE, BLACK);
+    }
+  }
+
   // PCに送信
-  Serial.printf("%7.3f,%7.3f,%7.3f,%7.3f,%7.3f,%7.3f\n", gx, gy, gz, ax, ay, az);
+  Serial.printf("%7.3f,%7.3f,%7.3f,%7.3f,%7.3f,%7.3f, %7.3f\n", gx, gy, gz, ax, ay, az, angle);
 
   // --- ボールの位置更新（gyに応じて左右移動） ---
   float scale = 0.1;
@@ -125,52 +147,89 @@ void loop()
     ballY = MAX_DISP_Y - 1;
 
   // 1フレーム前のボールを消去する
-  M5.Lcd.fillRect(prevBallX - BALL_RADIUS, prevBallY - BALL_RADIUS, BALL_RADIUS * 2 + 1, BALL_RADIUS * 2 + 1, BLACK);
-  int ballColor = RED;
-  if (controlEnable)
-  {
-    ballColor = GREEN;
-  }
-  M5.Lcd.fillCircle((int)ballX, (int)ballY, BALL_RADIUS, ballColor);
+  // M5.Lcd.fillRect(prevBallX - BALL_RADIUS, prevBallY - BALL_RADIUS, BALL_RADIUS * 2 + 1, BALL_RADIUS * 2 + 1, BLACK);
+  // int ballColor = RED;
+  // if (controlEnable)
+  // {
+  //   ballColor = GREEN;
+  // }
+  // M5.Lcd.fillCircle((int)ballX, (int)ballY, BALL_RADIUS, ballColor);
 
-  prevBallX = ballX;
-  prevBallY = ballY;
+  // prevBallX = ballX;
+  // prevBallY = ballY;
 
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(10, 10);
-  M5.Lcd.printf("gx: %7.3f dps\n", gx);
-  M5.Lcd.setCursor(10, 30);
-  M5.Lcd.printf("gy: %7.3f dps\n", gy);
-  M5.Lcd.setCursor(10, 50);
-  M5.Lcd.printf("gz: %7.3f dps\n", gz);
-
-  M5.Lcd.setCursor(10, 70);
-  M5.Lcd.printf("ax: %7.3f g\n", ax);
-  M5.Lcd.setCursor(10, 90);
-  M5.Lcd.printf("ay: %7.3f g\n", ay);
-  M5.Lcd.setCursor(10, 110);
-  M5.Lcd.printf("az: %7.3f g\n", az);
-
-  // 制御モードでない場合はここで終わり
   if (!controlEnable)
   {
-    return;
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("gx: %7.3f dps\n", gx);
+    M5.Lcd.setCursor(10, 30);
+    M5.Lcd.printf("gy: %7.3f dps\n", gy);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.printf("gz: %7.3f dps\n", gz);
+
+    M5.Lcd.setCursor(10, 70);
+    M5.Lcd.printf("ax: %7.3f g\n", ax);
+    M5.Lcd.setCursor(10, 90);
+    M5.Lcd.printf("ay: %7.3f g\n", ay);
+    M5.Lcd.setCursor(10, 110);
+    M5.Lcd.printf("az: %7.3f g\n", az);
+
+    M5.Lcd.setCursor(10, 130);
+    M5.Lcd.printf("angle: %7.3f deg\n", angle);
   }
 
-  dxl.setGoalPosition(RIGHT_LEG_ID, 205, UNIT_DEGREE);
-  dxl.setGoalPosition(LEFT_LEG_ID, 205, UNIT_DEGREE);
-  delay(1000);
+  // 目標確度からの差分
+  float error = target_angle - angle;
+  // 前回のエラーとの差
+  float derivative = error - last_error;
+  // 差分の積算値
+  integral += error;
+  // outは目標値に近いほど0になり、前に倒れるとマイナス、後ろに倒れるとプラスになる
+  float output = Kp * error + Ki * integral + Kd * derivative;
+  last_error = error;
 
-  // Set Goal Velocity using RPM
-  dxl.setGoalVelocity(RIGHT_WHL_ID, 60, UNIT_RPM);
-  dxl.setGoalVelocity(LEFT_WHL_ID, 60, UNIT_RPM);
-  delay(1000);
+  M5.Lcd.setCursor(10, 150);
+  M5.Lcd.printf("out: %7.3f deg\n", output);
 
-  dxl.setGoalPosition(RIGHT_LEG_ID, 245, UNIT_DEGREE);
-  dxl.setGoalPosition(LEFT_LEG_ID, 245, UNIT_DEGREE);
-  delay(1000);
+  output *= scale;
+  if (output > MAX_RPM)
+    output = MAX_RPM;
+  if (output < -MAX_RPM)
+    output = -MAX_RPM;
 
-  dxl.setGoalVelocity(RIGHT_WHL_ID, -60, UNIT_RPM);
-  dxl.setGoalVelocity(LEFT_WHL_ID, -60, UNIT_RPM);
-  delay(1000);
+  M5.Lcd.setCursor(10, 170);
+  M5.Lcd.printf("ctrl: %7.3f deg\n", output);
+
+  // 制御モードのときだけサーボを動かす
+  if (controlEnable)
+  {
+    dxl.setGoalVelocity(RIGHT_WHL_ID, output, UNIT_RPM);
+    dxl.setGoalVelocity(LEFT_WHL_ID, output, UNIT_RPM);
+  }
+
+  float now_time = millis();
+
+  M5.Lcd.setCursor(10, 190);
+  M5.Lcd.printf("interval: %7.3f ms\n", now_time - prev_time);
+
+  prev_time = now_time;
+
+  // テストモード
+  // dxl.setGoalPosition(RIGHT_LEG_ID, 205, UNIT_DEGREE);
+  // dxl.setGoalPosition(LEFT_LEG_ID, 205, UNIT_DEGREE);
+  // delay(1000);
+
+  // // Set Goal Velocity using RPM
+  // dxl.setGoalVelocity(RIGHT_WHL_ID, 60, UNIT_RPM);
+  // dxl.setGoalVelocity(LEFT_WHL_ID, 60, UNIT_RPM);
+  // delay(1000);
+
+  // dxl.setGoalPosition(RIGHT_LEG_ID, 245, UNIT_DEGREE);
+  // dxl.setGoalPosition(LEFT_LEG_ID, 245, UNIT_DEGREE);
+  // delay(1000);
+
+  // dxl.setGoalVelocity(RIGHT_WHL_ID, -60, UNIT_RPM);
+  // dxl.setGoalVelocity(LEFT_WHL_ID, -60, UNIT_RPM);
+  // delay(1000);
 }
